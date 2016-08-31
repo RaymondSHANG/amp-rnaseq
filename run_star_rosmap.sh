a427b38e65bbb2c20cd73bb7217414ea2ae06253
kkdang <kristen.dang@sagebase.org>
2016-08-31 18:47:14 -0400

    Removing R2 as an input.

--- run_star_rosmap.sh
+++ run_star_rosmap.sh
@@ -1,7 +1,6 @@
-! /bin/bash
+#! /bin/bash
 
 # $1 = R1.fastq
-# $2 = R2.fastq
 
 
 module load star/2.5.1b
@@ -9,9 +8,10 @@ module load star/2.5.1b
 
 index='/hpc/users/xdangk01/REFERENCES/GRCh38/star/Gencode24'
 prefix=`basename $1 .r1.fastq`
+r2=${1/r1.fastq/r2.fastq}
 
 mkdir /hpc/users/xdangk01/AMPAD/reprocess/outputs/ROSMAP/star/$prefix
 cd /hpc/users/xdangk01/AMPAD/reprocess/outputs/ROSMAP/star/$prefix
 
-STAR --runMode alignReads --runThreadN 6 --genomeDir $index --readFilesIn $1 $2 --outFileNamePrefix $prefix --outSAMtype BAM Unsorted --outSAMunmapped Within --quantMode GeneCounts --twopassMode Basic
+STAR --runMode alignReads --runThreadN 6 --genomeDir $index --readFilesIn $1 $r2 --outFileNamePrefix $prefix --outSAMtype BAM Unsorted --outSAMunmapped Within --quantMode GeneCounts --twopassMode Basic
 
