da133c3f02dc08cae9a5f020d410c2a5c683b0d6
James Eddy <james.a.eddy@gmail.com>
2017-02-24 15:19:18 -0800

    add command to combine all picard outputs; use temporary bam file instead of piping

--- run_picard.sh
+++ run_picard.sh
@@ -11,59 +11,53 @@ RIBOINTS=data/gencode.v23.rRNA.interval_list
 
 PICARD="/Users/jaeddy/anaconda/envs/picardtest/share/picard-2.7.1-2/picard.jar"
 
-# CollectAlignmentSummaryMetrics (after sorting)
+# Sort BAM
 java -Xmx8G -jar $PICARD SortSam \
     INPUT=data/$1 \
-    OUTPUT=/dev/stdout \
+    OUTPUT="data/${sample}.tmp.bam" \
     SORT_ORDER=coordinate \
-    QUIET=true \
     VALIDATION_STRINGENCY=SILENT \
-    COMPRESSION_LEVEL=0 \
-    | java -Xmx8G -jar $PICARD CollectAlignmentSummaryMetrics \
-        VALIDATION_STRINGENCY=LENIENT \
-        MAX_RECORDS_IN_RAM=4000000 \
-        ASSUME_SORTED=true \
-        ADAPTER_SEQUENCE= \
-        IS_BISULFITE_SEQUENCED=false \
-        MAX_INSERT_SIZE=100000 \
-        R=$FASTA \
-        INPUT=/dev/stdin \
-        OUTPUT="data/${sample}/picard.analysis.CollectAlignmentSummaryMetrics"
+    COMPRESSION_LEVEL=0
+
+# CollectAlignmentSummaryMetrics (after sorting)
+java -Xmx8G -jar $PICARD CollectAlignmentSummaryMetrics \
+    VALIDATION_STRINGENCY=LENIENT \
+    MAX_RECORDS_IN_RAM=4000000 \
+    ASSUME_SORTED=true \
+    ADAPTER_SEQUENCE= \
+    IS_BISULFITE_SEQUENCED=false \
+    MAX_INSERT_SIZE=100000 \
+    R=$FASTA \
+    INPUT="data/${sample}.tmp.bam" \
+    OUTPUT="data/${sample}/picard.analysis.CollectAlignmentSummaryMetrics"
 
 # CollectRnaSeqMetrics (after sorting)
-java -Xmx8G -jar $PICARD SortSam \
-    INPUT=data/$1 \
-    OUTPUT=/dev/stdout \
-    SORT_ORDER=coordinate \
-    QUIET=true \
-    VALIDATION_STRINGENCY=SILENT \
-    COMPRESSION_LEVEL=0 \
-    | java -Xmx8G -jar $PICARD CollectRnaSeqMetrics \
-        VALIDATION_STRINGENCY=LENIENT \
-        MAX_RECORDS_IN_RAM=4000000 \
-        STRAND_SPECIFICITY=NONE MINIMUM_LENGTH=500 \
-        RRNA_FRAGMENT_PERCENTAGE=0.8 \
-        METRIC_ACCUMULATION_LEVEL=ALL_READS \
-        R=$FASTA \
-        REF_FLAT=$REFFLAT \
-        RIBOSOMAL_INTERVALS=$RIBOINTS \
-        INPUT=/dev/stdin \
-        OUTPUT="data/${sample}/picard.analysis.CollectRnaSeqMetrics"
+java -Xmx8G -jar $PICARD CollectRnaSeqMetrics \
+    VALIDATION_STRINGENCY=LENIENT \
+    MAX_RECORDS_IN_RAM=4000000 \
+    STRAND_SPECIFICITY=NONE MINIMUM_LENGTH=500 \
+    RRNA_FRAGMENT_PERCENTAGE=0.8 \
+    METRIC_ACCUMULATION_LEVEL=ALL_READS \
+    R=$FASTA \
+    REF_FLAT=$REFFLAT \
+    RIBOSOMAL_INTERVALS=$RIBOINTS \
+    INPUT="data/${sample}.tmp.bam" \
+    OUTPUT="data/${sample}/picard.analysis.CollectRnaSeqMetrics"
 
 # MarkDuplicates (after sorting)
-java -Xmx8G -jar $PICARD SortSam \
-    INPUT=data/$1 \
-    OUTPUT=/dev/stdout \
-    SORT_ORDER=coordinate \
-    QUIET=true \
-    VALIDATION_STRINGENCY=SILENT \
-    COMPRESSION_LEVEL=0 \
-    | java -Xmx8G -jar $PICARD MarkDuplicates \
-        VALIDATION_STRINGENCY=LENIENT \
-        MAX_RECORDS_IN_RAM=4000000 \
-        ASSUME_SORTED=true \
-        REMOVE_DUPLICATES=true \
-        OPTICAL_DUPLICATE_PIXEL_DISTANCE=100 \
-        INPUT=/dev/stdin \
-        OUTPUT=/dev/null \
-        METRICS_FILE="data/${sample}/picard.analysis.MarkDuplicates"
+java -Xmx8G -jar $PICARD MarkDuplicates \
+    VALIDATION_STRINGENCY=LENIENT \
+    MAX_RECORDS_IN_RAM=4000000 \
+    ASSUME_SORTED=true \
+    REMOVE_DUPLICATES=false \
+    OPTICAL_DUPLICATE_PIXEL_DISTANCE=100 \
+    INPUT="data/${sample}.tmp.bam" \
+    OUTPUT=tmp.bam \
+    METRICS_FILE="data/${sample}/picard.analysis.MarkDuplicates"
+
+# Clean up
+rm "data/${sample}.tmp.bam"
+
+picard_outputs=$(find data/${sample} -name "picard.analysis*")
+./combine_picard_sample.py $picard_outputs \
+    -o "data/${sample}/picard.CombinedMetrics.csv"
