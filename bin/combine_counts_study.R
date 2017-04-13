98a175e899162f319496526ce9e0fbbf118f0c30
James Eddy <james.a.eddy@gmail.com>
2017-04-13 08:50:47 -0700

    clean up combine counts script; add combine metrics script

--- bin/combine_counts_study.R
+++ bin/combine_counts_study.R
@@ -1,6 +1,6 @@
 #! /usr/bin/env Rscript
 # JAE for Sage Bionetworks
-# Combine individual sample count files into a sample x gene matrix file.
+# Combine individual sample count files into a gene x sample matrix file.
 # April 12, 2017
 
 library(argparse)
@@ -10,9 +10,9 @@ library(tidyr)
 library(purrr)
 library(dplyr)
 
-parser = ArgumentParser(description = "Combine sample STAR count files into a matrix file.")
+parser = ArgumentParser(description = "Combine sample count files into a matrix file (rows of features by columns of samples).")
 parser$add_argument('file_list', type = "character", nargs = "+",
-                    help = "List of STAR read count files to combine.")
+                    help = "List of read count files to combine.")
 parser$add_argument('--out_prefix', type = "character", required = TRUE,
                     help = "Prefix for output files (i.e., <prefix>_all_counts_matrix.txt).")
 parser$add_argument('--sample_suffix', type = "character",
@@ -24,12 +24,18 @@ parser$add_argument('--col_num', default = 2, type = "integer",
                     help = "1-based index of counts column to select [default %(default)s].")
 args = parser$parse_args()
 
+# Extract sample names from file names
 names(args$file_list) <- map_chr(args$file_list, function(file_path) {
     basename(file_path) %>%
         str_replace(args$sample_suffix, "")
     })
+
+# Save the first column of the first file to a vector to use for ordering
+# rows in the combined matrix
 feature_order <- read_tsv(args$file_list[1], col_names = FALSE)[["X1"]]
 
+# Merge all columns into a single data frame; note: identifiers (e.g., gene
+# names) in the first column will be stored in the "feature" column
 message("Combining counts for all samples...")
 combined_counts <- map_df(args$file_list, function(file_path) {
     read_tsv(file_path, col_names = FALSE) %>%
@@ -40,6 +46,7 @@ combined_counts <- map_df(args$file_list, function(file_path) {
     .id = "sample") %>%
     spread(sample, count)
 
+# Write data frame as tab-delimited matrix file
 message("Saving output...")
 out_file <- file.path(args$out_dir,
                       paste0(args$out_prefix, "_all_counts_matrix.txt"))
